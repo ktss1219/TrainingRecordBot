310d2a8c09ab7d6e9640db090aa4e94f87bbb7cf
KaragiriRintaro <130040566+KaragiriRintaro@users.noreply.github.com>
2023-05-17 11:05:16 +0900

    関数の追加

--- src/reminder.py
+++ src/reminder.py
@@ -1,2 +1,20 @@
-#リマインド機能の詳細
-#あい
+def handle_message(event):
+    text = event.message.tevt
+
+    if text == "追加":
+        template_message = TemplateSendMessage(
+            alt_text='トレーニング曜日の選択',
+            template=ButtonsTemplate(
+                text='トレーニングは何曜日にしますか？',
+                actions=[
+                    PostbackTemplateAction(label='日曜日', data='Sunday'),
+                    PostbackTemplateAction(label='月曜日', data='Monday'),
+                    PostbackTemplateAction(label='火曜日', data='Tuesday'),
+                    PostbackTemplateAction(label='水曜日', data='Wednesday'),
+                    PostbackTemplateAction(label='木曜日', data='Thursday'),
+                    PostbackTemplateAction(label='金曜日', data='Friday'),
+                    PostbackTemplateAction(label='土曜日', data='Saturday')
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, template_message)
